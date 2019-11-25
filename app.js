86a345313327fabb5cf5651cd6f1f1b8111b168b
nlaw96 <lawrencianjume@gmail.com>
2019-11-25 12:25:49 +0000

    added new route

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 // fs.readFile("")
 const express = require('express');
 const app = express();
-const port = 6000;
+const port = 8000;
 const axios = require('axios');
-// const bodyParser = require('body-parser');
+const cors = require('cors')
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 app.use(express.static("public"));
 app.use(express.static("views"));
 
 // app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', `${__dirname}/views`)
 app.engine('ejs', require('ejs').renderFile);
@@ -19,8 +20,15 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => res.status(200).render('index'));
+app.get('/blogpost', (req, res) => res.status(200).render('blog'));
 
-app.get('database.json', (req, res) => {
+app.post('/newpost', (req, res) => { 
+    console.log(res.body)
+    console.log(res.body.textBody)
+    res.send(res.body.textBody)
+
+});
+app.get('/posts', (req, res) => {
     res.sendFile(path.join(__dirname, "database.json"));
 });
 
