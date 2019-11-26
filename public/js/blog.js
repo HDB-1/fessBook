c24d2c5a300758150ad86c6a2356f102d62fbbe0
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-11-26 11:09:41 +0000

    working gif selection

--- public/js/blog.js
+++ public/js/blog.js
@@ -22,18 +22,15 @@ $(document).ready(() => {
     .then((response) => {
       const url2 = response.data.data[0].images.downsized.url;
       $('#patrick_gif').attr("src", url2)
-       console.log(response.data.data[0].images.downsized.url)})
+      console.log(response.data.data[0].images.downsized.url)
+    })
 
   axios.get("https://api.giphy.com/v1/gifs?api_key=Vg9n7RBkEBnIpMlXbYQGvUjSXuOkOtdX&ids=UlqLDtI8Qc0j6")
     .then(
       (response) => {
         const url3 = response.data.data[0].images.downsized.url;
         $('#popcorn_gif').attr("src", url3)
-         console.log(response.data.data[0].images.downsized.url)})
-  // on submit button click
- 
-
-  // let bodyText =  $('textarea').val();
-  // let title = $title
+        console.log(response.data.data[0].images.downsized.url)
+      })
 
 });
