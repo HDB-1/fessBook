c2bb53255c6b8a4821cf7a65db7b80bb3ccaf82a
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-11-26 14:49:29 +0000

    gif required

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,5 @@
 // JS file for homepage
-function appendButtonHtmlGenerator(buttonType, buttonIndex){
+function appendButtonHtmlGenerator(buttonType, buttonIndex) {
     return `<button class="reactionButton" id=${buttonType}Button-${buttonIndex}>${buttonType}</button>`
 }
 $(document).ready(function () {
@@ -11,20 +11,25 @@ $(document).ready(function () {
             let textBody = postArray[i].textBody;
             let title = postArray[i].title;
             let gifHtml;
-            if(gif){
+            console.log(typeof gif);
+            console.log(gif);
+            if (gif.length > 2) {
                 gifHtml = `<img src="${gif}" />`;
-            }
-            else{
-                gifHtml ="";  
+                // console.log("has gif" + gif);
+                // console.log("has gif html" + gifHtml);
+            } else {
+                gifHtml = "";
+                // console.log("no gif" + gif);
+                // console.log("no gif HTML" + gifHtml);
             }
             let postHtml = `<div class="post" id="blog${[i]}">  ${title}  <br /> <p>${textBody}</p> <br /> ${gifHtml} </div>`;
             $("#blogPostContainer").append(postHtml)
-            
+
             // $("#blogPosts").append(`<div class="post" id="blog${[i]}">  ${title}  <br> ${textBody} <br> <button class="likeButton" id="likeButton_${i}>Like</button><button class="dislikeButton" id="dislikeButton_${i}>Dislike</button> <button class="laughterButton" id="laughterButton_${i}>Laugh</button> </div>`)
 
         }
-    
-        
+
+
 
     });
 })
